0e703656c1554127da00747f353f6acafe44bf54
leetmalware <126356875+leetmalware@users.noreply.github.com>
2025-04-26 12:18:35 -0700

    Delete deepseek_python_20250426_b3826f.py

--- deepseek_python_20250426_b3826f.py
+++
@@ -1,9 +0,0 @@
-question_bank = [
-    {
-        "question": "Which contributor to the Enlightenment identified two key aspects to our existence, liberty and property?",
-        "options": ["John Locke", "Thomas Hobbes", "Jean-Jacques Rousseau", "Voltaire"],
-        "answer": "John Locke"
-    },
-    # ... (all the other questions from your original code)
-    # I've omitted them here for brevity, but you should include all of them
-]
